c16c6cc404c185ebb8b884af2b53355ee54c0e89
RosarioManny <rosario.emm47@gmail.com>
2025-08-15 19:24:46 -0400

    Add Serializer File, Add Serializers for Models

---
+++ main_app/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from .models import Product, Property, CartItem, Cart, CustomOrder, Category
+
+# CATEGORY
+class CategorySerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = Category
+    fields = '__all__'
+
+# PRODUCT
+class ProductSerializer(serializers.ModelSerializer):
+  
+  category = CategorySerializer(read_only=True)
+
+  class Meta:
+    model = Product
+    fields = '__all__'
+    
+# PROPERTY
+class PropertySerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Property
+    fields = '__all__'
+# CART
+
+class CartSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = Cart
+    fields = '__all__'
+
+class ItemSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = CartItem
+    fields = ['id', 'product', 'quantity']
+
+# CUSTOM 
+class CustomOrderSerializer(serializers.ModelSerializer):
+  
+  class Meta: 
+    model = CustomOrder
+    fields = '__all__'
