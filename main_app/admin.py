73b32594a56fbd748f5f87d740c70cab075d0d70
RosarioManny <rosario.emm47@gmail.com>
2025-08-15 21:06:33 -0400

    Admin Registration

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+from django.contrib import admin 
+from .models import Product, Property,Category, CartItem, Cart, CustomOrder
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(CustomOrder)
+admin.site.register(Category)
+admin.site.register(Property)
