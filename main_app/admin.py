a3d8ebe2729532e71a41b1bac726a30674d7c803
RosarioManny <rosario.emm47@gmail.com>
2025-09-13 17:13:57 -0400

    Image preview in Admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -24,6 +24,11 @@ class ProductAdmin(admin.ModelAdmin):
     filter_horizontal = ['properties']
     
     # Add a method to display primary image in list view
+    def image_preview(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" style="max-height: 100px; max-width: 100px;" />')
+        return "No Image"
+
     def get_primary_image(self, obj):
         primary_image = obj.images.filter(is_primary=True).first()
         if primary_image:
@@ -33,7 +38,6 @@ class ProductAdmin(admin.ModelAdmin):
         if first_image:
             return mark_safe(f'<img src="{first_image.image.url}" style="max-height: 50px; max-width: 50px;" />')
         return "No Image"
-    get_primary_image.short_description = 'Image'
     
     # Optional: Show properties in list view
     def get_properties_list(self, obj):
