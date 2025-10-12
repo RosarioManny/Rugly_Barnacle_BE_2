6ed5dc9e010bfc7ebb8848babaf6d2eaee0b9134
RosarioManny <rosario.emm47@gmail.com>
2025-10-12 14:35:42 -0400

    Deployment -- HealthCheck URL

--- rbproduct/urls.py
+++ rbproduct/urls.py
@@ -2,9 +2,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "healthy", "message": "Django is running"})
+
 urlpatterns = [ 
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls'))
+    path('', include('main_app.urls')),
+    path('health/', health_check)
 ]
 
 if settings.DEBUG:
