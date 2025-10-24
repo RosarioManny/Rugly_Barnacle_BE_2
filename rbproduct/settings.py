cf5f85c6fb318f31c6415049a2bc2bf66ce3c6bb
RosarioManny <rosario.emm47@gmail.com>
2025-10-24 16:59:39 -0400

    Code clean up

--- rbproduct/settings.py
+++ rbproduct/settings.py
@@ -58,7 +58,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # ADD THIS LINE
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -91,7 +91,6 @@ WSGI_APPLICATION = 'rbproduct.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import dj_database_url
 
 DATABASES = {
     'default': {
@@ -161,7 +160,7 @@ SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_SAMESITE = 'None'
 SESSION_SAVE_EVERY_REQUEST = True
-# SESSION_COOKIE_DOMAIN = '.onrender.com'
+
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
@@ -198,7 +197,6 @@ CSRF_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = False
 CSRF_COOKIE_NAME = 'csrftoken'
-# CSRF_COOKIE_DOMAIN = '.onrender.com'
 CSRF_USE_SESSIONS = False
 
 
